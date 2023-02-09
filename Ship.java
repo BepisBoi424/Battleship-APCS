7354b678b6f2e699f267712c372e04f626c67b8c
BepisBoi424 <jasper.ward2005@gmail.com>
2023-02-09 09:05:14 -0700

    Fix classes

--- Ship.java
+++ Ship.java
@@ -1,3 +1,10 @@
 public class Ship {
+    public static int ships;
+
+
+    public void Carrier() {
+        ships += 1;
+    }
+
     
 }
--- Tile.java
+++ Tile.java
@@ -2,6 +2,14 @@ public class Tile {
     public boolean isOccupied;
     
     public Tile() {
-        
+        isOccupied = false;
+    }
+
+    public void Board() {
+        Tile[][] board = new Tile[10][10];
+    }
+
+    public void PrintBoard() {
+        for(int row = 0; )
     }
 }
