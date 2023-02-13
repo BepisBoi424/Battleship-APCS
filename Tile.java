c3321c8304b6f1619b194d1221a0c4b0d286e56e
BepisBoi424 <jasper.ward2005@gmail.com>
2023-02-13 12:26:18 -0700

    Created board

--- Tile.java
+++ Tile.java
@@ -1,15 +1,29 @@
-public class Tile {
-    public boolean isOccupied;
+public class Tile 
+{
+    private boolean isOccupied;
+    private String[][] board;
     
-    public Tile() {
+    public Tile() 
+    {
         isOccupied = false;
+        board = new String[10][10];
     }
 
-    public void Board() {
-        Tile[][] board = new Tile[10][10];
-    }
-
-    public void PrintBoard() {
-        for(int row = 0; )
+    public void printBoard() 
+    {
+        System.out.println("    A     B     C     D     E     F     G     H     I     J");
+        for(int i = 0; i < board.length; i++) 
+        {
+            System.out.print(i + " ");
+            for(int j = 0; j < board[i].length; j++) 
+            {
+                if(board[i][j] == null) 
+                {
+                    board[i][j] = "[ O ] ";
+                }
+                System.out.print(board[i][j]);
+            }
+            System.out.println();
+        }
     }
 }
